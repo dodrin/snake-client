314957efe63a1f36c571f333dfb5935ba4cfe86e
Rina Inada <indrn@icloud.com>
2023-06-18 02:44:57 +0000

    add comments

--- play.js
+++ play.js
@@ -1,7 +1,14 @@
 const net = require("net");
+
+//handles different aspects of the network connection to the server where the game runs
 const { connect } = require("./client");
+
+//sets up the input handling for user interaction via stdin
 const { setupInput } = require("./input.js")
 
 console.log("Connecting ...");
 const conn = connect();
 setupInput(conn);
+
+// connect();
+// setupInput();
